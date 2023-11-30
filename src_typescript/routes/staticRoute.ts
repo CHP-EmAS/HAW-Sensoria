bc961c2f0ad0a27b480f9f29e125ebf99fa1d8ef
Clemens Hübner <clemens.huebner@innogames.com>
2023-11-30 14:54:13 +0100

    remove 404 page

--- src_typescript/routes/staticRoute.ts
+++ src_typescript/routes/staticRoute.ts
@@ -10,8 +10,4 @@ router.use('/img', express.static('static/images'));
 
 router.use("/", express.static("static/html/index.html"));
 
-router.use("*", function(request: Request, response: Response) {
-    response.status(404).sendFile(path.join(__dirname + '/../../static/html/404.html'));
-});
-
 export default router;
