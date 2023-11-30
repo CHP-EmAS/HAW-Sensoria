5a0b9de10d8e17ff62f0f3163bcc95b79d1dbd07
Clemens Hübner <clemens.huebner@innogames.com>
2023-11-30 16:00:03 +0100

    test

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,12 +1,12 @@
 import '../css/style.css'
 
-import * as Three from 'three'
+import * as THREE  from '../../node_modules/three/build/three'
 
-const scene = new Three.Scene();
+const scene = new THREE .Scene();
 
-const camera = new Three.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+const camera = new THREE .PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
 
-const renderer = new Three.WebGLRenderer({
+const renderer = new THREE .WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
@@ -16,9 +16,9 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new Three.TorusGeometry(10, 3, 16, 100)
-const material = new Three.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
-const torus = new Three.Mesh( geometry, material);
+const geometry = new THREE .TorusGeometry(10, 3, 16, 100)
+const material = new THREE .MeshBasicMaterial({color: 0xFF6347, wireframe: true});
+const torus = new THREE .Mesh( geometry, material);
 
 scene.add(torus);
 
