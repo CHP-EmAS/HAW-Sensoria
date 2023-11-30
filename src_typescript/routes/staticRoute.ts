0d4cf6e1ca6b8ecf110e176982fb269166e99227
Clemens Hübner <clemens.huebner@innogames.com>
2023-11-30 15:23:22 +0100

    test

--- src_typescript/routes/staticRoute.ts
+++ src_typescript/routes/staticRoute.ts
@@ -9,7 +9,7 @@ router.use('/script', express.static('static/script'));
 router.use('/img', express.static('static/images'));
 
 router.get("/", function(req, res) {
-    res.sendFile('/static/html/index.html');
+    res.sendFile(path.join(__dirname + '/../../static/html/index.html'));
 });
 
 export default router;
