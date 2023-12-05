e28e5d96616049d1dc0bbf3b6d55e5f7b236d722
EmAS <clemens@huebner-clan.de>
2023-12-06 00:32:22 +0100

    test

--- src_typescript/routes/staticRoute.ts
+++ src_typescript/routes/staticRoute.ts
@@ -14,12 +14,12 @@ router.get("/Build/:file", function(req, res) {
         } else {
             var fileExt = req.params.file.split('.').pop();
 
-            
-                res.header(
+            if(fileExt == "br") {
+                res.setHeader(
                     "Content-Encoding",
                     "br",
                 );
-            
+            }
             
           res.contentType(req.params.file);
           res.send(data);
