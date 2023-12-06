e17da5b81253f1fc549cdc1561cd36944f495ba0
EmAS <clemens@huebner-clan.de>
2023-12-06 20:01:43 +0100

    new unity build

--- src_typescript/routes/staticRoute.ts
+++ src_typescript/routes/staticRoute.ts
@@ -7,27 +7,6 @@ const router = Router();
 router.use('/Build', express.static('static/build'));
 router.use('/TemplateData', express.static('static/templates'));
 
-router.get("/Build/:file", function(req, res) {
-    fs.readFile('/../../static/build/' + req.params.file, function(err, data) {
-        if(err) {
-          res.send("Oops! Couldn't find that file.");
-        } else {
-            var fileExt = req.params.file.split('.').pop();
-
-           
-                res.setHeader(
-                    "Content-Encoding",
-                    "br",
-                );
-            
-            
-          //res.contentType(req.params.file);
-          res.send(data);
-        }   
-        res.end();
-      }); 
-});
-
 router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + '/../../static/html/index.html'));
 });
